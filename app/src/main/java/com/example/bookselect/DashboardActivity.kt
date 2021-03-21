f8703396827825140aa023f61f6df8dbb5e3bff6
3567 <mokalpooja380@gmail.com>
2021-03-21 18:48:54 +0530

    commit

---
+++ app/src/main/java/com/example/bookselect/DashboardActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bookselect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+
+        id_txt.text = currentUser?.uid
+        name_txt.text = currentUser?.displayName
+        email_txt.text = currentUser?.email
+    }
+}
---
+++ app/src/main/java/com/example/bookselect/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bookselect
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        Handler().postDelayed({
+            if(user!=null){
+                val dashboardIntent = Intent(this,DashboardActivity::class.java)
+                startActivity(dashboardIntent)
+                finish()
+            }else{
+                val signInIntent = Intent(this,SignInActivity::class.java)
+                startActivity(signInIntent)
+            }
+        },1000)
+    }
+}
